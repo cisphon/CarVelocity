24a1e8758e015f5a13ded739fa81238225cf1509
Andre Castillo <castilloandre@tutamail.com>
2020-04-24 19:53:33 -0500

    Added new classes to make objects from Weather is enum Cases is enum VelocityCalculator is empty right now IndicentData will be a class to store values

---
+++ src/Cases.java
@@ -0,0 +1,5 @@
+public enum Cases {
+    SKID,
+    YAW,
+    AIRBORNE
+}
---
+++ src/IndicentData.java
@@ -0,0 +1,23 @@
+public class IndicentData {
+
+    final double GRAVITY = 9.80665;
+    final double RADIUS_OF_EARTH = 6731009;
+
+    Weather weather;
+
+    Cases cases;
+
+    double altitude;
+
+    double mph;
+
+    double final_velocity;
+
+    double skid_distance;
+
+    double skid;
+
+    double radius;
+
+    double superElevation;
+}
