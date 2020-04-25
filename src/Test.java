4fbe5c1947b1b1cb486cb9744166c886e8116287
RonnyeA <59902995+RonnyeA@users.noreply.github.com>
2020-04-24 21:23:54 -0500

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -148,11 +148,69 @@ public class Test extends DragFactor {
         return num;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         IncidentData data = new IncidentData();
         VelocityCalculator vCalc = new VelocityCalculator();
         
+    	//login block
+    	//creates the scanner, file(if doesn't already exist), bufferedreader, filewriter, and filereader
+    	File loginDatabase = new File("src/loginDatabase");
+   		loginDatabase.createNewFile();
+  		FileWriter writer = new FileWriter(loginDatabase, true);
+		FileReader file_reader = new FileReader(loginDatabase);
+    	BufferedReader br = new BufferedReader(file_reader);
+    			
+    	//asks for username
+    	System.out.println("Please enter login information: ");
+    	System.out.print("Username: ");
+    	String uName = sc.next();
+
+    	//checks database to see if username already exists in database
+    	int sCase = 0;
+    	String line = br.readLine();
+    	while(line != null) {
+    		if (line.contains(uName)) {
+    			sCase = 1;
+    			break;
+    		}
+    		line = br.readLine();
+    	}
+    			
+    	String password;
+    	String updated;
+    	boolean valid = false;
+    			
+    	//prompts user to create a new account if non existent or simply enter password to continue
+    	switch(sCase) {
+    	case 0:
+    		System.out.println("Username not found!");
+    		System.out.println("Enter a password to create an account with username '" + uName + "'");
+    		System.out.print("Password: ");
+    		password = sc.next();
+    		updated = uName + "," + password;
+    		writer.write(updated + System.lineSeparator());
+    		writer.close();
+    		System.out.println("New account created!");
+    		break;
+
+    	case 1:
+    		while(valid != true) {
+    		System.out.print("Password: ");
+    		password = sc.next();
+    		String [] temp = line.split(",");
+    			if(temp[1].contains(password)) {
+    				System.out.println("Access Granted");
+    				valid = true;
+    			}
+    			else {
+    				System.out.println("Incorrect password, try again");
+    			}
+    		}
+    	}
+    	br.close();
+    	//login block ends
+         
         System.out.println("Please select case number:");
         System.out.println("1.Skid Case");
         System.out.println("2.Yaw Case");
