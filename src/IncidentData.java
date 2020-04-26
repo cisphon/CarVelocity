654e3076ec32634f1af5311ff5d5ee8d5edce398
Dangcherry <64285892+Dangcherry@users.noreply.github.com>
2020-04-25 21:01:16 -0500

    Added times and Hypotenuse

--- src/IncidentData.java
+++ src/IncidentData.java
@@ -1,76 +1,88 @@
+
 public class IncidentData {
+	  final double GRAVITY = 9.80665;
+	    final double RADIUS_OF_EARTH = 6731009;
 
-    final double GRAVITY = 9.80665;
-    final double RADIUS_OF_EARTH = 6731009;
+	    Weather weather;
 
-    Weather weather;
+	    RoadType roadType;
 
-    RoadType roadType;
+	    int incidentCase;
 
-    int incidentCase;
+	    double altitude;
 
-    double altitude;
+	    double mph;
+	    
+	    double skidTime;
+	    
+	    double yawTime;
+	    
+	    double airborneTime;
 
-    double mph;
+	    double finalVelocity;
 
-    double finalVelocity;
+	    double skidDistance;
 
-    double skidDistance;
+	    double radius;
 
-    double radius;
+	    double superElevation;
 
-    double superElevation;
+	    double horizontalDistance;
 
-    double horizontalDistance;
+	    double verticalDistance;
+	    
+	    double hypotenuseDistance;
+	    
 
-    double verticalDistance;
-
-    void setRoadType(int caseNum) {
-        switch (caseNum) {
-            case 1:
-                roadType = RoadType.CEMENT_NEW;
-                break;
-            case 2:
-                roadType = RoadType.CEMENT_TRAVELLED;
-                break;
-            case 3:
-                roadType = RoadType.CEMENT_POLISHED;
-                break;
-            case 4:
-                roadType = RoadType.ASPHALT_NEW;
-                break;
-            case 5:
-                roadType = RoadType.ASPHALT_TRAVELLED;
-                break;
-            case 6:
-                roadType = RoadType.ASPHALT_POLISHED;
-                break;
-            case 7:
-                roadType = RoadType.ASPHALT_EXCESS;
-                break;
-            case 8:
-                roadType = RoadType.GRAVEL_PACKED;
-                break;
-            case 9:
-                roadType = RoadType.GRAVEL_LOOSE;
-                break;
-            case 10:
-                roadType = RoadType.CINDERS_PACKED;
-                break;
-            case 11:
-                roadType = RoadType.ROCK_CRUSHED;
-                break;
-            case 12:
-                roadType = RoadType.ICE_SMOOTH;
-                break;
-            case 13:
-                roadType = RoadType.SNOW_PACKED;
-                break;
-            case 14:
-                roadType = RoadType.SNOW_LOOSE;
-                break;
-        }
+	    void setRoadType(int caseNum) {
+	        switch (caseNum) {
+	            case 1:
+	                roadType = RoadType.CEMENT_NEW;
+	                break;
+	            case 2:
+	                roadType = RoadType.CEMENT_TRAVELLED;
+	                break;
+	            case 3:
+	                roadType = RoadType.CEMENT_POLISHED;
+	                break;
+	            case 4:
+	                roadType = RoadType.ASPHALT_NEW;
+	                break;
+	            case 5:
+	                roadType = RoadType.ASPHALT_TRAVELLED;
+	                break;
+	            case 6:
+	                roadType = RoadType.ASPHALT_POLISHED;
+	                break;
+	            case 7:
+	                roadType = RoadType.ASPHALT_EXCESS;
+	                break;
+	            case 8:
+	                roadType = RoadType.GRAVEL_PACKED;
+	                break;
+	            case 9:
+	                roadType = RoadType.GRAVEL_LOOSE;
+	                break;
+	            case 10:
+	                roadType = RoadType.CINDERS_PACKED;
+	                break;
+	            case 11:
+	                roadType = RoadType.ROCK_CRUSHED;
+	                break;
+	            case 12:
+	                roadType = RoadType.ICE_SMOOTH;
+	                break;
+	            case 13:
+	                roadType = RoadType.SNOW_PACKED;
+	                break;
+	            case 14:
+	                roadType = RoadType.SNOW_LOOSE;
+	                break;
+	        }
 
 
-    }
-}
+	    }
+	}
+
+
+
